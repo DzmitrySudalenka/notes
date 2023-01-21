22b19a5361123e65aa41787572efb9cf7d0bde09
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-22 01:17:37 +0300

    feat: add note component

---
+++ src/components/Note.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  note: {
+    type: Object,
+    required: true,
+    default: () => {}
+  }
+})
+</script>
+
+<template>
+  <div class="note">{{note.name}}</div>
+</template>
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,9 +1,14 @@
 <script setup>
-  import { useNotesStore } from '@/stores/notes'
+import { useNotesStore } from '@/stores/notes'
+import Note from './Note.vue'
 
-  const notesStore = useNotesStore()
+const notesStore = useNotesStore()
 </script>
 
 <template>
-  {{notesStore.notes}}
+  <Note 
+    v-for='note of notesStore.notes'
+    :key='note.id'
+    :note='note'
+  />
 </template>
