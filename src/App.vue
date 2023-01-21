a738858178c48d9f8df3653bc15f57db93f03613
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-21 18:19:51 +0300

    feat: add bootstrap grid

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
-import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
-  <header>
-    header
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col logo-wrap">
+          <RouterLink to="/" class="logo">
+            <img class="logo__icon" src="@/assets/logo.svg" alt="logo" width="32" height="32" />
+            Notes
+          </RouterLink>
+        </div>
+      </div>
+    </div>
   </header>
   
   <RouterView />
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -3,7 +3,13 @@ import Notes from '../components/Notes.vue'
 </script>
 
 <template>
-  <main>
-    <Notes />
+  <main class="main">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Notes />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
