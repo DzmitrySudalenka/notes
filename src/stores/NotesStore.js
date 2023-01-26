60d91cfc632f461552e288629c67afc666397bf8
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-27 00:42:39 +0300

    feat: add state saving to localstorage

--- src/stores/NotesStore.js
+++ src/stores/NotesStore.js
@@ -1,55 +1,11 @@
 import { defineStore } from 'pinia'
+import { useLocalStorage } from '@vueuse/core'
+import { demoData } from './NotesStoreDemoData'
+
+const state = localStorage.getItem('state') ? localStorage.getItem('state') : demoData
 
 export const useNotesStore = defineStore("notesStore", {
-  state: () => ({
-    nextNoteId: 3,
-    notes: [
-      {
-        id: 1,
-        name: 'First note',
-        nextTaskId: 4,
-        tasks: [
-          {
-            id: 1,
-            completed: true,
-            text: 'First note First task',
-          },
-          {
-            id: 2,
-            completed: false,
-            text: 'First note Second task'
-          },
-          {
-            id: 3,
-            completed: true,
-            text: 'First note Third task'
-          }
-        ]
-      },
-      {
-        id: 2,
-        name: 'Second note',
-        nextTaskId: 4,
-        tasks: [
-          {
-            id: 1,
-            completed: false,
-            text: 'Second note First task'
-          },
-          {
-            id: 2,
-            completed: true,
-            text: 'Second note Second task'
-          },
-          {
-            id: 3,
-            completed: false,
-            text: 'Second note Third task'
-          }
-        ]
-      }
-    ]
-  }),
+  state: () => (useLocalStorage('state', {state})),
   actions: {
     addNote(note) {
       note.id = this.nextNoteId++
