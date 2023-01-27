13c4f78ac1dda58bbf780e4607acdf1aeabba42d
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-27 17:38:11 +0300

    feat: add build

--- src/components/Notes.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import NoteForm from './NoteForm.vue'
-import NotesList from './NotesList.vue'
-</script>
-
-<template>
-  <RouterLink to="/add" class="note-btn">Add new</RouterLink>
-  <NotesList />
-</template>
